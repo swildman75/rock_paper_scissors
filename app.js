156057cc3a808222fd152a33ecd46ba381a10f2c
Sean Wildman <sean.wildman75@gmail.com>
2023-04-21 20:15:13 -0400

    Add variables for selecting DOM Elements

--- app.js
+++ app.js
@@ -2,6 +2,20 @@
 let playerScore = 0;
 let computerScore = 0;
 
+// let playerSelection;
+
+// Variables to select elements
+const buttons = document.querySelectorAll('.btn');
+
+const player = document.querySelector('#player_score');
+player.textContent = `Player Score: ${playerScore}`;
+
+const computer = document.querySelector('#computer_score');
+computer.textContent = `Computer Score: ${computerScore}`;
+
+const output = document.querySelector('#output');
+output.textContent = "Let's See Who Can Win Five Matches.";
+
 // Function to get computers random choice
 const getComputerChoice = () => {
   let randChoice = Math.floor(Math.random() * 3);
@@ -14,52 +28,64 @@ const getComputerChoice = () => {
   }
 };
 
-// Function to get players choice
-const getPlayerChoice = () => {
-  let playerChoice = prompt('Rock, Paper or Scissors?').toLowerCase();
-  return playerChoice;
-};
-
 // function to check winner
 const checkWinner = (playerSelection, computerSelection) => {
   if (playerSelection === computerSelection) {
+    output.textContent = "It's a draw!";
     return "It's a draw";
   } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+    output.textContent = `You win, ${playerSelection} beats ${computerSelection}`;
     playerScore++;
-    return `You win, ${playerSelection} beats ${computerSelection}`;
   } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    output.textContent = `You win, ${playerSelection} beats ${computerSelection}`;
     playerScore++;
-    return `You win, ${playerSelection} beats ${computerSelection}`;
   } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+    output.textContent = `You win, ${playerSelection} beats ${computerSelection}`;
     playerScore++;
-    return `You win, ${playerSelection} beats ${computerSelection}`;
   } else {
+    output.textContent = `You Lose, ${computerSelection} beats ${playerSelection}`;
     computerScore++;
-    return `You lose, ${computerSelection} beats ${playerSelection}`;
   }
 };
+
+// Loop through button and set event listener on click event
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    playerSelection = button.id;
+    computerSelection = getComputerChoice();
+    checkWinner(playerSelection, computerSelection);
+  });
+});
 
 // Function to play one round
 const playRound = () => {
-  let playerSelection = getPlayerChoice();
   let computerSelection = getComputerChoice();
-  let winner = checkWinner(playerSelection, computerSelection);
-  console.log(winner);
+  // let winner = checkWinner(playerSelection, computerSelection);
+  // return winner;
 };
+playRound();
+
+// // Function to play one round
+// const playRound = () => {
+//   let computerSelection = getComputerChoice();
+//   let winner = checkWinner(playerSelection, computerSelection);
+//   return winner;
+// };
 
 // Function to play a five round game
 const game = () => {
-  while (playerScore < 5 && computerScore < 5) {
-    playRound();
-    console.log(`Player score: ${playerScore}`);
-    console.log(`Computer score: ${computerScore}`);
-  }
+  player.textContent = `Player Score: ${playerScore}`;
+  computer.textContent = `Computer Score: ${computerScore}`;
   if (playerScore === 5) {
-    console.log('You win this game!');
+    output.textContent = 'You Won the Game!';
+    player.textContent = `Player Score: ${playerScore}`;
+    computer.textContent = `Computer Score: ${computerScore}`;
+  } else if (computerScore === 5) {
+    output.textContent = 'Computer Wins the Game!';
+    player.textContent = `Player Score: ${playerScore}`;
+    computer.textContent = `Computer Score: ${computerScore}`;
   }
-  if (computerScore === 5) {
-    console.log('The computer wins the game!');
-  }
+  playRound();
 };
 
-// game();
+game();
